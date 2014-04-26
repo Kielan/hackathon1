3784120c0fd65d287609e78231147156c8b88fea
Adam Bliss <abliss@gmail.com>
2014-04-26 07:20:20 +0000

    oops missing file

--- meetup.js
+++ meetup.js
@@ -3,12 +3,16 @@
 // abliss's personal key... will be turned off someday
 var API_KEY = "5d1d2266525715f473340c66482f68";
 
-var meetup=require('meetup-api')(API_KEY);
+var meetup = require('meetup-api')(API_KEY);
+var fs = require('fs');
 
 meetup.getOpenEvents(
-    {'topic' : 'hackathon',
-     'city': 'portland'
+    {'text' : 'hackers',
+     'zip': '97201'
     },
-    function(err,events) {
-        console.log(events);
+    function(err,resp) {
+        fs.writeFileSync('meetup-events.js', JSON.stringify(resp));
+        resp.results.forEach(function(ev) { 
+            console.log(ev.name);
+        });
     });
